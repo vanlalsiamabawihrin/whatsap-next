89f12b54fc2ccb3ec11f356300c88dffe030fe50
siama2017 <siama2017@users.noreply.github.com>
2021-04-16 15:44:24 +1000

    inital comment

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const Sidebar = () => {
+  return (
+    <Container>
+      <h1>Sidebar</h1>
+    </Container>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div``;
